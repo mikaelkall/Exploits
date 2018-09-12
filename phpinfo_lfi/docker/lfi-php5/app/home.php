c34b1208eabab26faab2e9c8efe034752ee05427
Mikael Kall <kall.micke@gmail.com>
2018-09-12 16:14:54 +0200

    Added some more documentation regarding phpinfo_lfi.py

---
+++ phpinfo_lfi/docker/lfi-php5/app/home.php
@@ -0,0 +1,13 @@
+<html>
+<head></head>
+<body>
+
+<img src='troll.jpg'></a><br/><br/>
+
+<pre>
+   This application is vurnerable for file inclusion.<br/><br/>
+   http://localhost:8080/index.php?file=<filename>
+</pre>
+
+</body>
+</html>
